01077974925b24e5f8b89e249f3a24ab3fad377b
Facu <facumatiu@hotmail.com>
2019-10-04 20:39:06 -0300

    Actualizado la busqueda por filtro en trabajo

--- Trabajo/InfoAPI/genresAPI.php
+++ Trabajo/InfoAPI/genresAPI.php
@@ -1,31 +1,31 @@
-<?php 
+<?php
 
 namespace InfoAPI;
 
-class genresAPI implements IgenresAPI{
-     
+class genresAPI implements IgenresAPI
+{
 
-    function getGenres()
+
+    static function getGenres()
     {
-    $jsonMoviesGenres=file_get_contents("https://api.themoviedb.org/3/genre/movie/list?language=en-US&api_key=f74ffe2d8ab6690478568c0a2eb5582a");
-    $GenresDetails=json_decode($jsonMoviesGenres);
-    //LE ASIGNO A UN ATRIBUTO O LO DEJO ASÍ
-    return $GenresDetails->genres;
+        $jsonMoviesGenres = file_get_contents("https://api.themoviedb.org/3/genre/movie/list?language=en-US&api_key=f74ffe2d8ab6690478568c0a2eb5582a");
+        $GenresDetails = json_decode($jsonMoviesGenres);
+
+        return $GenresDetails->genres;
     }
 
-    function getIDGenre($AllGenres,$genre)
+    static function getIDGenre($genre)
     {
-    $results=null;
-    for ($i=0; $i < count($AllGenres); $i++)
-    { 
-        if ($AllGenres[$i]->name==$genre)
-        {
-            return $AllGenres[$i]->id;
+        $results = null;
+       
+        $AllGenres = genresAPI::getGenres();
+        for ($i = 0; $i < count($AllGenres); $i++) {
+
+            if ($AllGenres[$i]->name == $genre) {
+                return $AllGenres[$i]->id;
+                break;
+            }
         }
-    }   
-    return $results;
+        return $results;
     }
 }
-
-
-?>
---
+++ Trabajo/process/searchMoviesWithFilters.php
@@ -0,0 +1,24 @@
+<?php
+
+require_once("../config/autoload.php");
+
+use config\autoload as Autoload;
+Autoload::Start();
+
+use InfoAPI\genresAPI as genresAPI;
+use InfoAPI\moviesAPI as moviesAPI;
+
+
+$moviesAPI=new moviesAPI();
+
+if (isset($_GET['genres'])) {
+   
+$movies=moviesAPI::getMoviesFromApi();
+$MovieGenres=genresAPI::getGenres();
+$Genres=$_GET['genres'];
+
+$moviesWithGenres=moviesAPI::getMovieForGenres($Genres,$movies);
+var_dump($moviesWithGenres);
+}
+
+?>
