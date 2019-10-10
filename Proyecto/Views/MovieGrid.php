cb0e0788dae7c9262ad770ece882dc327fc813ae
Matías Nicoletti <38164832+MatiasNicoletti@users.noreply.github.com>
2019-10-10 15:56:22 -0300

    Moviegrid

---
+++ Proyecto/Views/MovieGrid.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/MovieStyle.css">
+    <title>Document</title>
+</head>
+
+<body>
+    <div class="container">
+        <?php
+        for ($i = 0; $i < 9; $i++) {
+            ?>
+            <div class="block"></div>
+        <?php
+        }
+        ?>
+
+    </div>
+</body>
+
+</html>
