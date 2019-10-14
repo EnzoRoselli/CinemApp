638bed4b2a119a4ddf511df88c392d3077207d92
Matías Nicoletti <38164832+MatiasNicoletti@users.noreply.github.com>
2019-10-14 13:05:35 -0300

    views

--- Proyecto/Views/header.php
+++ Proyecto/Views/header.php
@@ -13,10 +13,16 @@
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css">
     
     <script class="cssdeck" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/1.8.0/jquery.min.js"></script> -->
+    
+
+    <link rel="stylesheet" href="./css/bootstrap.min.css">
+	  <link rel="stylesheet" type="text/css" href="css/main.css"> 
     <link href="https://fonts.googleapis.com/css?family=Montserrat:300,400,600|Open+Sans" rel="stylesheet">
     <link rel="stylesheet" href="./css/style.css">
     <link rel="stylesheet" href="./css/cineView.css">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css">
+
+
     <script src='js/SearchMovie.js'></script>
     <title>CinemApp</title>
   </head>
