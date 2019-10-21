cfb6286f06bbe43d857b2d3ff9e942efd0e36cab
Matías Nicoletti <38164832+MatiasNicoletti@users.noreply.github.com>
2019-10-21 13:06:13 -0300

    config mayus

--- Config/config.php
+++ Config/config.php
@@ -8,10 +8,9 @@ define("JSON", ROOT . "/data");
 //Path to your project's root folder
 
 // FRONT PATH
-define("FRONT_ROOT", "http://localhost/Final-Tp-Lab4");
-// define("FRONT_ROOT", "http://localhost/Proyecto");
+//define("FRONT_ROOT", "http://localhost/Final-Tp-Lab4");
 //PARA LA COMPU DE MATI 
-// define("FRONT_ROOT", "http://localhost:90/Final-Tp-Lab4");
+define("FRONT_ROOT", "http://localhost:90/Final-Tp-Lab4");
 define("VIEWS_PATH", FRONT_ROOT . "/Views");
 define("CSS_PATH", VIEWS_PATH . "/css");
 define("JS_PATH", VIEWS_PATH . "/js");
@@ -29,7 +28,8 @@ define("IMG_PATH", VIEWS_PATH . "/img");
 define("DB_HOST","localhost");
 define("DB_USER","root");
 define("DB_NAME","CINEMAPP");
- define("DB_PASS","");
+//define("DB_PASS","");
+define("DB_PASS","root");
 
 
 
