5036d1e73b9b9a014365e59bc492ebf7dfb1b874
Matías Nicoletti <38164832+MatiasNicoletti@users.noreply.github.com>
2019-10-27 18:29:35 -0300

    login fixed

--- Views/LoginSignup.php
+++ Views/LoginSignup.php
@@ -3,6 +3,9 @@
 namespace Views;
 
 
+include('header.php');
+// include('nav.php');
+
 
 
 //require 'app/fb_init.php';
@@ -17,76 +20,49 @@ namespace Views;
 	}
 } ?>
 
-
 <body class="login-body">
 	<h1>CinemApp</h1>
-	<div class="login-container" id="container">
+	<div class="container" id="container">
 		<div class="form-container sign-up-container">
-			<form action="<?php echo FRONT_ROOT . "/User/createUser" ?>" method="POST" class="login-form">
+			<form action=<?php echo FRONT_ROOT . "/User/createUser" ?> method="POST" class="login-form">
 				<h2>Create Account</h2>
 				<div class="social-container">
 					<!-- <a href="#" class="social"><i class="fab fa-facebook-f"></i></a> -->
 
 				</div>
-				<!-- <span>or use your email for registration</span> -->
+				<span>or use your email for registration</span>
 				<input class="imput-login" name="SignupName" type="text" placeholder="Name" />
 				<input class="imput-login" name="SignupLastName" type="text" placeholder="Last Name" />
 				<input class="imput-login" name="SignupDNI" type="text" placeholder="ID Number" />
 				<input class="imput-login" name="SignupEmail" type="email" placeholder="Email" />
 				<input class="imput-login" name="SignupPassword" type="SignupPassword" placeholder="Password" />
-				<button class="login-button">Sign Up</button>
+				<button class="login-btn">Sign Up</button>
 			</form>
 		</div>
 		<div class="form-container sign-in-container">
-			<form action="<?php echo FRONT_ROOT . "/User/loginAction" ?>" method="POST" class="login-form">
+        <form action=<?php echo FRONT_ROOT . "/User/loginAction" ?> method="POST" class="login-form">
 				<h2>Log in</h2>
-				<!-- <div class="social-container"> -->
-
-				<?php
-				/*	if (isset($_SESSION['facebook_access_token'])) {
-					$fb->setDefaultAccessToken($_SESSION['facebook_access_token']);
-					try {
-						$response = $fb->get('/me');
-						$userNode = $response->getGraphUser();
-					} catch (Facebook\Exceptions\FacebookResponseException $e) {
-						echo "graph returned an error: " . $e->getMessage();
-						exit;
-					} catch (Facebook\Exceptions\FacebookSDKException $e) {
-						echo "SDK returned an error: " . $e->getMessage();
-						exit;
-					}
-					echo "Logged in as " . $userNode->getName();
-					session_start();
-					$_SESSION['loggedUser']=$userNode->getName();
-					header("Location: ../views/home.php");
-					echo '<br/> <a href="logout.php"> Log out</a>';
-				} else {
-					$helper = $fb->getRedirectLoginHelper();
-					$permissions = []; // Optional permissions
-					$loginUrl = $helper->getLoginUrl('http://localhost/Final-Tp-Lab4/Final-Tp-Lab4/Proyecto/Login/login-callback.php', $permissions);
-
-					echo '<a href="' . htmlspecialchars($loginUrl) . '">Log in with Facebook!</a>';
-				}*/ ?>
-
-				<!-- </div> -->
-				<span class="login-span">or use your account</span>
-				<input class="imput-login" name="LoginEmail" type="LoginEmail" placeholder="Email" />
-				<input class="imput-login" name="LoginPassword" type="LoginPassword" placeholder="Password" />
-				<a href="ForgotPassword.html" class="login-a">Forgot your password?</a>
-				<button class="login-button">Log In</button>
+				<div class="social-container">
+					<!-- <a href="#" class="social"><i class="fab fa-facebook-f"></i></a> FIJARSE EL LOIGIN DE FB -->
+				</div>
+				<span>or use your account</span>
+				<input type="LoginEmail" placeholder="Email" />
+				<input type="LoginPassword" placeholder="Password" />
+				<a href="ForgotPassword.html">Forgot your password?</a>
+				<button class="login-btn">Log In</button>
 			</form>
 		</div>
 		<div class="overlay-container">
 			<div class="overlay">
 				<div class="overlay-panel overlay-left">
-					<h2>Welcome Back!</h2>
-					<p class="login-p">To keep connected with us please login with your info</p>
-					<button class="ghost" id="signIn">Sign In</button>
+					<h1>Welcome Back!</h1>
+					<p>To keep connected with us please login with your info</p>
+					<button class="ghost login-btn" id="signIn">Sign In</button>
 				</div>
 				<div class="overlay-panel overlay-right">
-					<h2>Hello!</h2>
-					<p class="login-p">Enter your personal details and welcome to CinemApp</p>
-					<button class="ghost login-button" id="signUp">Sign Up</button>
+					<h1>Hello!</h1>
+					<p>Enter your personal details and welcome to CinemApp</p>
+					<button class="ghost login-btn" id="signUp">Sign Up</button>
 				</div>
 			</div>
 		</div>
@@ -100,5 +76,6 @@ namespace Views;
 		<a target="_blank" href="https://www.florin-pop.com/blog/2019/03/double-slider-sign-in-up-form/">here</a>.
 	</p>
 </footer> -->
-	<script src=<?php echo "js/scripts.js" ?>></script>
+
+<script src=<?php echo JS_PATH . "/login.js" ?>></script>
 </body>
--- Views/header.php
+++ Views/header.php
@@ -4,19 +4,16 @@
 
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-	  <link rel="stylesheet" type="text/css" href="<?php echo CSS_PATH ?>/main.css"> 
     <link href="https://fonts.googleapis.com/css?family=Montserrat:300,400,600|Open+Sans" rel="stylesheet">
     <link rel="stylesheet" href="<?php echo CSS_PATH ?>/style.css">
-
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css">
     <link rel="stylesheet" href="<?php echo CSS_PATH ?>/table.css">
     <link rel="stylesheet" href="<?php echo CSS_PATH ?>/navStyle.css">
     <link rel="stylesheet" href="<?php echo CSS_PATH ?>/mainStyle.css">
-    <link rel="stylesheet" href="<?php echo CSS_PATH ?>/popup.css">
+    <!-- <link rel="stylesheet" href="<?php echo CSS_PATH ?>/popup.css"> -->
     <link rel="stylesheet" href="<?php echo CSS_PATH ?>/GridStyle.css">
     <link rel="stylesheet" href="<?php echo CSS_PATH ?>/filters.css">
-    <link rel="stylesheet" href="<?php echo CSS_PATH ?>/loginstyles.css">
+    <link rel="stylesheet" href="<?php echo CSS_PATH ?>/login.css">
     <script src='js/SearchMovie.js'></script>
     <title>CinemApp</title>
   </head>
