d603e96b7cf7c7c788ad6967fc4f64826aceedd6
matias <matiasnicolettimdp@gmail.com>
2019-10-31 14:54:38 -0300

    Todo vacio

--- Views/footer.php
+++
@@ -1,6 +0,0 @@
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <!-- <script src="js/jquery.min.js"></script> -->
-    <script src="<?php echo JS_PATH . "/bootstrap.min.js" ?></script>
-  </body>
-</html>
--- Views/logout.php
+++
@@ -1,4 +0,0 @@
-<?php 
-session_start();
-unset($_SESSION['facebook_access_token']);
-header('Location: LoginSignup.php');
