5ba0b432ad85156a6d2e0b8212132e8655f0aa72
MateuGit <facumatiu@hotmail.com>
2019-11-10 17:55:09 -0300

    Update ShowtimeController.php

--- Controllers/ShowtimeController.php
+++ Controllers/ShowtimeController.php
@@ -194,7 +194,7 @@ class ShowtimeController
         $this->showShowtimeMenu();
     }
 
-    public function desactivate($id)
+    public function desactivate()
     {
         $id = $_GET['desactivate'];
         try {
