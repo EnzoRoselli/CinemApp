92f86efa26c8d0c2595540cc824b6df4c4dfce77
MateuGit <facumatiu@hotmail.com>
2019-11-13 19:42:11 -0300

    Tickets new atributes

--- Model/Ticket.php
+++ Model/Ticket.php
@@ -8,21 +8,28 @@ class Ticket{
     private $ticketId;
     private $qr_code;
     private $ticketNumber;
+    private $showtime;
+    private $purchase;
 
 
-    public function __construct($ticketId,$qr_code,$ticketNumber){
+    public function __construct($ticketId="",$qr_code="",$ticketNumber="",$purchase=""){
         $this->ticketId=$ticketId;
         $this->qr_code=$qr_code;
         $this->ticketNumber=$ticketNumber;
+        $this->purchase=$purchase;
     }
 
     public function getId(){ return $this->id;}
     public function getQr_Code(){return $this->qr_code;}
     public function getTicketNumber(){return $this->ticketNumber;}
+    public function getShowtime(){return $this->showtime;}
+    public function getPurchase(){return $this->purchase;}
 
     public function setId($id){$this->id = $id;}
     public function setQr_Code($qr_code){$this->qr_code=$qr_code;}
     public function setTicketNumber($ticketNumber){$this->ticketNumber=$ticketNumber;}
+    public function setShowtime($showtime){$this->showtime=$showtime;}
+    public function setPurchase($purchase){$this->purchase=$purchase;}
 }
 
 
