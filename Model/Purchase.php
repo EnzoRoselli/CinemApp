6b23b0c968f50f01fb6b073b3a31a6e1b60c2f4f
matias <matiasnicolettimdp@gmail.com>
2019-11-13 19:18:07 -0300

    -

--- Model/Purchase.php
+++ Model/Purchase.php
@@ -12,15 +12,16 @@ class Purchase{
     private $grossValue; //valor bruto
     private $discount;
     private $id;
- 
+    private $user;
 
-    public function __construct($date="", $hour="",$ticketAmount="",$total="",$discount="", $grossValue=""){
+    public function __construct($date="", $hour="",$ticketAmount="",$total="",$discount="", $grossValue="", $user=""){
         $this->date=$date;
         $this->hour=$hour;
         $this->ticketAmount=$ticketAmount;
         $this->total=$total;
         $this->$discount=$discount;
         $this->$grossValue=$grossValue;
+        $this->$user=$user;
         
     }
 
@@ -31,6 +32,7 @@ class Purchase{
     public function getTotal(){return $this->total;}
     public function getDiscount(){return $this->discount;}
     public function getGrossValue(){return $this->grossValue;}
+    public function getUser(){return $this->user;}
 
     public function setDate($date){$this->date=$date;}
     public function setHour($hour){$this->hour=$hour;}
@@ -38,6 +40,7 @@ class Purchase{
     public function setTotal($total){$this->total=$total;}
     public function setDiscount($discount){$this->discount=$discount;}
     public function setGrossValue($grossValue){$this->grossValue=$grossValue;}
+    public function setUser($user){$this->user=$user;}
 }
 
 
