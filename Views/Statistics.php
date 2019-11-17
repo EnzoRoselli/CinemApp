5bb50ca1e45194c36d904177ddd1fa32383aa377
matias <matiasnicolettimdp@gmail.com>
2019-11-17 14:28:54 -0300

    stats

--- Views/Statistics.php
+++ Views/Statistics.php
@@ -7,17 +7,46 @@ include('nav.php');
 <body class="admin-body">
 	<div class="admin-header">
         <h1>Statistics</h1>
-        <h2>Cinemas</h2>
+        
 	</div>
-
+	
 	<div class="admin-table">
-
+	<h2>Cinemas</h2>
 		<table class="content-table">
 
 			<thead>
 				<tr>
 					<th style="width: 3%;">Cinema</th>
 					<th style="width: 10%;">Address</th>
+					<th style="width: 20%;">Tickets sold</th>
+					<th style="width: 24%;">Amount</th>
+				</tr>
+			</thead>
+			<tbody>
+				<?php
+				// foreach
+						?>
+						<tr>
+
+							<td><?php //echo $cine->getId(); ?></td>
+							
+							<td><?php //echo $cine->getCapacity(); ?></td>
+							
+						</tr>
+				<?php 
+				 ?>
+			</tbody>
+		</table>
+	</div>
+	
+	<div class="admin-table">
+	<h2>Movies</h2>
+		<table class="content-table">
+
+			<thead>
+				<tr>
+					<th style="width: 3%;">Name</th>
+					<th style="width: 10%;">Release Date</th>
 					<th style="width: 20%;">Tickets sold</th>
 					<th style="width: 24%;">Amount</th>
 				</tr>
