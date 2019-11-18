0fe641896f2779be17ad0f81db10e0f53263c025
calandrajose <calandrajosei@gmail.com>
2019-11-18 15:13:02 -0300

    cambio constantes usuario

--- Config/Constants/UserConstants.php
+++ Config/Constants/UserConstants.php
@@ -1,12 +1,12 @@
 <?php 
     namespace Config\Constants;
 
-    if (!defined('SIGNUP_SUCCESS')) define('SIGNUP_SUCCESS', '"Usuario creado exitosamente, por favor inicie sesion con sus credenciales"');
-    if (!defined('WRONG_CREDENTIALS')) define('WRONG_CREDENTIALS', '"Usuario o contraseña incorrectos"');
-    if (!defined('SIGNUP_FAILURE')) define('SIGNUP_FAILURE', '"Los datos que intenta ingresar corresponden a un usuario existente en nuestra base de datos"');
-    if (!defined('INCOMPLETE_INPUTS')) define('INCOMPLETE_INPUTS', '"Por favor ingrese todos los campos"');
-    if (!defined('LOGOUT_SUCCESS')) define('LOGOUT_SUCCESS', '"La sesion se ha cerrado con éxito"');
-    if (!defined('EMAIL_DOMAIN_ERROR')) define('EMAIL_DOMAIN_ERROR', '"El dominio ingresado no es valido"');
+    if (!defined('SIGNUP_SUCCESS')) define('SIGNUP_SUCCESS', '" Your profile has been succesfully created. Please login using your new credentials"');
+    if (!defined('WRONG_CREDENTIALS')) define('WRONG_CREDENTIALS', '"Either the username or password you entered are incorrect"');
+    if (!defined('SIGNUP_FAILURE')) define('SIGNUP_FAILURE', '"The information you are providing already exists in database "');
+    if (!defined('INCOMPLETE_INPUTS')) define('INCOMPLETE_INPUTS', '"All fields needs to be filled"');
+    if (!defined('LOGOUT_SUCCESS')) define('LOGOUT_SUCCESS', '"You had succesfully logged out"');
+    if (!defined('EMAIL_DOMAIN_ERROR')) define('EMAIL_DOMAIN_ERROR', '"Your email domain is not valid"');
 
 
 ?>
